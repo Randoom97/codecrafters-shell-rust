5c08bfc3a4e9b10257a214ed10cfb2b344362f65
lterry <randoom97@live.com>
2025-04-29 11:02:17 -0600

    the type builtin: executable files

--- src/commands.rs
+++ src/commands.rs
@@ -1,4 +1,4 @@
-use std::process::exit;
+use std::{env, path::PathBuf, process::exit};
 
 pub fn parse_command(input: &str) -> Option<Command> {
     let command_parts: Vec<&str> = input.trim().split_whitespace().collect();
@@ -7,18 +7,30 @@ pub fn parse_command(input: &str) -> Option<Command> {
         return None;
     }
 
-    return Some(match command_parts[0] {
-        "exit" => Command::Exit, // might need the input later to change the exit code
-        "echo" => Command::Echo(input.trim()[4..].trim().to_owned()),
-        "type" => Command::Type(parse_command(input.trim()[4..].trim()).map(|sc| Box::new(sc))),
-        _ => Command::InvalidCommand(input.trim().to_owned()),
-    });
+    match command_parts[0] {
+        "exit" => Some(Command::Exit), // might need the input later to change the exit code
+        "echo" => Some(Command::Echo(input.trim()[4..].trim().to_owned())),
+        "type" => Some(Command::Type(
+            parse_command(input.trim()[4..].trim()).map(|sc| Box::new(sc)),
+        )),
+        _ => {
+            let paths = env::var_os("PATH").unwrap();
+            for path in env::split_paths(&paths) {
+                let exec_path = path.join(command_parts[0]);
+                if exec_path.is_file() {
+                    return Some(Command::Executable(exec_path));
+                }
+            }
+            return Some(Command::InvalidCommand(input.trim().to_owned()));
+        }
+    }
 }
 
 pub enum Command {
     Exit,
     Echo(String),
     Type(Option<Box<Command>>),
+    Executable(PathBuf),
     InvalidCommand(String),
 }
 
@@ -32,6 +44,7 @@ impl Command {
                     println!("{}", subcommand.as_ref().unwrap().r#type());
                 }
             }
+            Command::Executable(..) => {}
             Command::InvalidCommand(input) => println!("{}: command not found", input.trim()),
         }
     }
@@ -41,6 +54,7 @@ impl Command {
             Command::Echo(..) | Command::Exit | Command::Type(..) => {
                 format!("{} is a shell builtin", self.name())
             }
+            Command::Executable(path) => format!("{} is {}", self.name(), path.display()),
             Command::InvalidCommand(input) => format!("{}: not found", input.trim()),
         };
     }
@@ -50,6 +64,7 @@ impl Command {
             Command::Exit => "exit",
             Command::Echo(..) => "echo",
             Command::Type(..) => "type",
+            Command::Executable(path) => path.file_name().unwrap().to_str().unwrap(),
             Command::InvalidCommand(..) => "invalid_command",
         };
     }
