865823dda3f279b8afeef9b9a3e8d85dd8fcd2cd
lterry <randoom97@live.com>
2025-05-08 13:40:31 -0600

    executable completion

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-#[allow(unused_imports)]
-use std::io::{self, Write};
+use std::env;
 
 mod commands;
 mod parser;
 
 fn main() {
     let mut editor = rustyline::Editor::new().unwrap();
-    editor.set_helper(Some(Completer));
+    editor.set_helper(Some(Completer::new()));
 
     loop {
         let input = editor.readline("$ ").unwrap();
@@ -19,7 +18,36 @@ fn main() {
 }
 
 #[derive(rustyline::Helper, rustyline::Highlighter, rustyline::Hinter, rustyline::Validator)]
-struct Completer;
+struct Completer {
+    complete_options: Vec<String>,
+}
+impl Completer {
+    pub fn new() -> Completer {
+        let mut complete_options: Vec<String> = Vec::new();
+
+        // TODO Tie this more closely with the enum in commands.rs
+        let builtins = vec!["echo", "exit", "type", "pwd", "cd"];
+        complete_options.append(&mut builtins.iter().map(|s| s.to_string()).collect());
+
+        let paths = env::var_os("PATH").unwrap();
+        for path in env::split_paths(&paths) {
+            if path.is_file() {
+                complete_options.push(path.file_name().unwrap().to_str().unwrap().to_string());
+                continue;
+            }
+            if path.is_dir() {
+                let dir = path.read_dir().unwrap();
+                for entry_option in dir {
+                    let entry = entry_option.unwrap();
+                    if entry.metadata().unwrap().is_file() {
+                        complete_options.push(entry.file_name().into_string().unwrap());
+                    }
+                }
+            }
+        }
+        Completer { complete_options }
+    }
+}
 impl rustyline::completion::Completer for Completer {
     type Candidate = String;
 
@@ -29,13 +57,10 @@ impl rustyline::completion::Completer for Completer {
         _pos: usize,
         _ctx: &rustyline::Context<'_>,
     ) -> Result<(usize, Vec<String>), rustyline::error::ReadlineError> {
-        // TODO Tie this more closely with the enum in commands.rs
-        let builtins = vec!["echo", "exit", "type", "pwd", "cd"];
-
         let mut options = Vec::new();
-        for builtin in builtins {
-            if builtin.starts_with(line) {
-                options.push(builtin.to_string() + " ");
+        for complete_option in &self.complete_options {
+            if complete_option.starts_with(line) {
+                options.push(complete_option.clone() + " ");
             }
         }
 
